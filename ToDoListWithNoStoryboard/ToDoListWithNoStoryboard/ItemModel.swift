130552b706e48cc295a7362dfe7508274538967b
TonyJem <a.jemanov@gmail.com>
2021-02-16 15:46:35 +0200

    Update ItemModel.swift

--- ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ItemModel.swift
+++ ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ItemModel.swift
@@ -1,1 +1,21 @@
 import Foundation
+
+struct TodoItem {
+    var title: String?
+    var isMarkedDone = false
+}
+
+class TodoItemModel {
+    
+    static func getTodoItems() -> [TodoItem] {
+        let todoItems = [
+            TodoItem(title: "TodoItem1", isMarkedDone: false),
+            TodoItem(title: "TodoItem2", isMarkedDone: false),
+            TodoItem(title: "TodoItem3", isMarkedDone: false),
+            TodoItem(title: "TodoItem4", isMarkedDone: false),
+            TodoItem(title: "TodoItem5", isMarkedDone: false),
+            TodoItem(title: "TodoItem6", isMarkedDone: false)
+        ]
+        return todoItems
+    }
+}
