2577cff2f285d081d12e9d7bc64c37b716fefd10
TonyJem <a.jemanov@gmail.com>
2021-02-16 19:06:22 +0200

    Minor Rename of "navBarFontColor"

--- ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/AppColors.swift
+++ ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/AppColors.swift
@@ -2,6 +2,6 @@ import UIKit
 
 struct AppColors {
     static var backgroundColor: UIColor = UIColor(red: 0.2431372549, green: 0.7647058824, blue: 0.8392156863, alpha: 1)
-    static var todoItemTitleFontColor: UIColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static var navBarFontColor: UIColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
     static let tableCellSeparatorColor: UIColor = .gray
 }
