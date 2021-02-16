2230dc137cc411671c5009d6bddcd2298e10ec2b
TonyJem <a.jemanov@gmail.com>
2021-02-16 15:28:42 +0200

    Add "UITableViewDataSource" and "UITableViewDelegate" protocols to "ToDoListViewController"

--- ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ToDoListViewController.swift
+++ ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ToDoListViewController.swift
@@ -1,3 +1,8 @@
+// Used Resources:
+//https://softauthor.com/ios-uitableview-programmatically-in-swift/
+
+
+
 import UIKit
 
 class ToDoListViewController: UIViewController {
@@ -7,4 +12,19 @@ class ToDoListViewController: UIViewController {
 
     }
 
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
