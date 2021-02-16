5a45f12e442de62b134b643ad429516d7e6da3b3
TonyJem <a.jemanov@gmail.com>
2021-02-16 20:28:37 +0200

    Add to ItemTableViewCell "checkMark" image

--- ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ItemTableViewCell.swift
+++ ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ItemTableViewCell.swift
@@ -7,16 +7,17 @@ class ItemTableViewCell: UITableViewCell {
             guard let currentItem = todoItem else {return}
             if let title = currentItem.title {
                 titleLabel.text = title
+                checkMark.image = UIImage(named: "Unchecked")
             }
         }
     }
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 20)
-        label.textColor = AppColors.itemTitleFontColor
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    let checkMark: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill // image will never be strecthed vertially or horizontally
+        image.translatesAutoresizingMaskIntoConstraints = false // enable autolayout
+        image.clipsToBounds = true
+        return image
     }()
     
     private let containerView: UIView = {
@@ -26,25 +27,37 @@ class ItemTableViewCell: UITableViewCell {
         return view
     }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textColor = AppColors.itemTitleFontColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        self.contentView.addSubview(checkMark)
         containerView.addSubview(titleLabel)
         self.contentView.addSubview(containerView)
         
+        checkMark.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+        checkMark.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 20).isActive = true
+        checkMark.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        checkMark.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
         containerView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
-        containerView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: self.checkMark.trailingAnchor, constant: 10).isActive = true
         containerView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
-        containerView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 25).isActive = true
         
         titleLabel.topAnchor.constraint(equalTo: self.containerView.topAnchor).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor, constant: 20).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor).isActive = true
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
 }
