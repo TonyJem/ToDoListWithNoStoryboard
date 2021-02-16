e767aaf52c1c1319f5179fa68e0e123f78c7e8a3
TonyJem <a.jemanov@gmail.com>
2021-02-16 15:51:25 +0200

    Connect model to ToDoListViewController

--- ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ToDoListViewController.swift
+++ ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ToDoListViewController.swift
@@ -4,10 +4,11 @@ https://softauthor.com/ios-uitableview-programmatically-in-swift/
 https://ioscoachfrank.com/remove-main-storyboard.html
 */
 
-
 import UIKit
 
 class ToDoListViewController: UIViewController {
+    
+    private let todoItems = TodoItemModel.getTodoItems()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +19,16 @@ class ToDoListViewController: UIViewController {
 
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return todoItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = todoItems[indexPath.row].title
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+        return 50
     }
 }
