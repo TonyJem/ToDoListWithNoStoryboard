b341f5266fec63c6f3b6ce072def36b56b6656b4
TonyJem <a.jemanov@gmail.com>
2021-02-16 18:50:40 +0200

    Add "titleLabel" to "ItemTableViewCell"

--- ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ItemTableViewCell.swift
+++ ToDoListWithNoStoryboard/ToDoListWithNoStoryboard/ItemTableViewCell.swift
@@ -1,5 +1,41 @@
 import UIKit
 
 class ItemTableViewCell: UITableViewCell {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textColor = UIColor(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true // this will make sure its childrenViews do not go out of the boundary
+        return view
+    }()
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        containerView.addSubview(titleLabel)
+        self.contentView.addSubview(containerView)
+
+        containerView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: self.containerView.topAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor).isActive = true
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+        
 }
